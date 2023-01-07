5234a956de86f0a27948b7457896d2628378a653
matt <mat@unc.edu>
2023-01-07 00:45:54 -0500

    working thing

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ function App() {
       <button style={{top:"20%",left:"5%",opacity:"0.8",backgroundColor:"cyan",borderRadius:"10%",fontSize:"150%",height:"10%",width:"20%"}} onClick={(e) => setArtist('Fortune')}>Fortune</button>
       <div style={style}></div>
       <input style={{opacity:"0.7",position:"fixed",top:"80%",width:"60%",left:"5%",height:"10%",fontSize:"10px",borderRadius:"2%",border:"10px solid lightgray",backgroundColor:"lightblue"}} type="text" value={search} placeholder={`Ask ${background} Anything...`} onChange={(e) => setSearch(e.target.value)} />
-      <button onClick={ask} style={{position:"fixed",top:"81%",width:"24%",left:"72%",fontSize:"40px",height:"10%",border:"10px solid lightgray"}}>🔥</button>
+      <button onClick={ask} style={{position:"fixed",top:"81%",width:"24%",left:"72%",fontSize:"40px",height:"10%",border:"10px solid lightgray"}} backgroundColor={fireColor}>🔥</button>
       <div style={{position:"fixed",left:"5%",top:"25%",height:"50%", fontSize:"15px",zIndex:"99",width:"70%"}}>{response}</div>
     </div>
   );
